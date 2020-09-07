509290e705cadd8429ae2865af770000203455c2
Iván Guevara <ivan@interconectando.com>
2020-09-07 00:38:22 +0000

    Planes

---
+++ blackline/blackline/doctype/planes/planes.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Interconectando, Desarrollando y Automatizando, SAPI de CV and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Planes', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ blackline/blackline/doctype/planes/test_planes.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Interconectando, Desarrollando y Automatizando, SAPI de CV and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestPlanes(unittest.TestCase):
+	pass
